8ecaf9fb348bae90fb523c7441ed8297b8f43a67
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-04-05 17:54:41 -0400

    Ejercicio 03
    Finalizacion de ejercicio 03
    
    Problemas con un punto y coma xD

--- Ejercisio 02.cpp
+++ Ejercisio 02.cpp
@@ -1,2 +1,24 @@
 /* Escribe un programa que lea de la entrada estandar el precio de un producto
-y muestre en la salida estandar el precio del producto al aplicarle el Iva*"
+y muestre en la salida estandar el precio del producto al aplicarle el Iva*/
+
+#include<iostream>
+
+using namespace std;
+
+int main () {
+	int producto;
+	int  valorTotal;
+	float iva;
+	
+	cout<<"Bienvenido a la Calculadora de IVA";
+	cout<<"\nIngrese el precio de su producto: "; cin>>producto;
+	//cout<<"Ingrese el valor del IVA: "; cin>>iva;
+	
+	iva = producto *0.19;
+	valorTotal = producto + iva;
+	
+	cout<<"El precio total de su producto mas IVA es de: "<<valorTotal;
+	
+	
+	return 0;
+}
